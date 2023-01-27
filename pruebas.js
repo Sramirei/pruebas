586176ca0cf196e2c430964f323ef4e5c3053ca4
Sramirei <seebaastiian24@gmail.com>
2023-01-27 15:15:52 -0500

    Correccion de errores

--- pruebas.js
+++
@@ -1,9 +0,0 @@
-const { countries } = require('./data')
-
-const country = countries.find(country => country.countryName === 'Colombi')
-
-if (country === undefined) {
-  console.log('vacio')
-} else {
-  console.log(country)
-}
